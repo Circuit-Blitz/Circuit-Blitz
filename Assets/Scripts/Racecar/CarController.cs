be64911bd7957b0f4dfdfe3d4432d82e2aa2ac67
Sublimeful <Qiangwu2000@gmail.com>
2024-03-08 00:21:19 -0600

    Added check for ground, made it so that car cannot turn unless car is moving and grounded

--- Assets/Scripts/Racecar/CarController.cs
+++ Assets/Scripts/Racecar/CarController.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public Rigidbody RB;
-    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 100f;
+    public float forwardAccel = 8f, reverseAccel = 4f, maxSpeed = 50f, turnStrength = 100f, gravityForce = 10f;
     
     private float speedInput, turnInput;
+    
+    private bool grounded;
+    
+    public LayerMask whatIsGround;
+    public float groundRayLength = 0.5f;
+    public Transform groundRayPoint;
     
     // Start is called before the first frame update
     void Start()
@@ -29,14 +35,31 @@ public class CarController : MonoBehaviour
         
         turnInput = Input.GetAxis("Horizontal");
         
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime, 0));
+        if (grounded) {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0));
+        }
         
         transform.position = RB.transform.position;
     }
     
     private void FixedUpdate() {
-        if (Mathf.Abs(speedInput) > 0) {
-            RB.AddForce(transform.forward * speedInput);
+        
+        grounded = false;
+        
+        RaycastHit hit;
+        
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround)) {
+            grounded = true;
+        }
+        
+        Debug.Log(grounded);
+        
+        if (grounded) {
+            if (Mathf.Abs(speedInput) > 0) {
+                RB.AddForce(transform.forward * speedInput);
+            }
+        } else {
+            RB.AddForce(Vector3.up * -gravityForce * 100f);
         }
     }
 }
