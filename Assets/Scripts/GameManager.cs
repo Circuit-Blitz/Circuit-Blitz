ab571b291667bb543fe4d9e658e52134bd37b00a
Sublimeful <Qiangwu2000@gmail.com>
2024-03-09 23:57:52 -0600

    Made a main menu scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : NetworkBehaviour
 {
     [SerializeField] private Transform PlayerPrefab;
-    
-    public override void OnNetworkSpawn() {
-        if (IsClient && IsOwner) {
-            SpawnCarRpc(NetworkManager.LocalClientId);
 
-            // Disable the UI's camera
-            Destroy(GameObject.Find("UICamera"));
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer) {
+            NetworkManager.SceneManager.OnSceneEvent += SceneManager_OnSceneEvent;
         }
+    }
+
+    void Awake() {
+        // Don't destroy this GameObject when a scene is loaded
+        DontDestroyOnLoad(this);
+    }
+    
+    public void LoadMap(String mapName) {
+        NetworkManager.SceneManager.LoadScene(mapName, LoadSceneMode.Single);
+    }
+
+    private void SceneManager_OnSceneEvent(SceneEvent sceneEvent) {
+        switch (sceneEvent.SceneEventType) {
+            case SceneEventType.LoadComplete:
+            {
+                if (sceneEvent.ClientId == NetworkManager.ServerClientId)
+                {
+                    // Spawn the car for players when the scene is loaded
+                    StartGameRpc();
+                }
+                break;
+            }
+        }
+    }
+
+    [Rpc(SendTo.Everyone)]
+    public void StartGameRpc() {
+        SpawnCarRpc(NetworkManager.LocalClientId);
     }
     
     [Rpc(SendTo.Server)]
     void SpawnCarRpc(ulong localPlayerId) {
-        if (IsServer) {
-            // Instantiate on server
-            Transform Player = Instantiate(PlayerPrefab);
-            Player.GetComponent<NetworkObject>().SpawnWithOwnership(localPlayerId);
-        }
+        // Instantiate on server
+        Transform Player = Instantiate(PlayerPrefab);
+        Player.GetComponent<NetworkObject>().SpawnWithOwnership(localPlayerId);
     }
 }
---
+++ Assets/Scripts/UserInterface/MainMenuUI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
